4097ad2241ddc1e63d867b16feeba729d686f5bd
DobDaniel <david.dobondi@moisenicoaraonline.ro>
2021-09-13 21:50:39 +0300

    Added sidemenu and overlay functionality with js

--- script.js
+++ script.js
@@ -16,3 +16,33 @@ const scrollDown = (e) => {
 };
 
 learnmore.addEventListener("click", scrollDown);
+
+let hamburger = document.getElementById("hamburger");
+let sidemenu = document.getElementById("sidemenu");
+let overlay = document.getElementById("overlay");
+
+const openSide = (e) => {
+    e.preventDefault();
+    overlay.style.display = "block";
+    sidemenu.style.width = "250px";
+
+};
+const closeSide = (e) => {
+    e.preventDefault();
+    
+    if (e.layerX > 251) {
+        sidemenu.style.width = "0px";
+        overlay.style.display = "none";
+    }
+};
+
+hamburger.addEventListener("click", openSide);
+document.addEventListener("click", closeSide);
+
+window.addEventListener("resize", function(e){
+    var width = window.innerWidth;
+    if(width>767){
+        sidemenu.style.width = "0px";
+        overlay.style.display = "none";
+    }
+})
