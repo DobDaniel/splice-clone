5accf6703c9e9e6356d9916c2a4d72788c78c1d5
DobDaniel <david.dobondi@moisenicoaraonline.ro>
2021-09-13 20:07:00 +0300

    bugs fixed & added js functionality

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ let cookiebutton = document.getElementById("cookiesbutton");
 
 const removeCookieBar = (e) => {
     e.preventDefault();
-    cookiebar.style.setProperty('display','none','important')
+    cookiebar.style.setProperty("display", "none", "important");
 };
 
 cookiebutton.addEventListener("click", removeCookieBar);
+
+let learnmore = document.getElementById("learn-more");
+
+const scrollDown = (e) => {
+    e.preventDefault();
+    window.scroll({ top: 900, behavior: "smooth" });
+};
+
+learnmore.addEventListener("click", scrollDown);
